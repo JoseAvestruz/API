21ff1df97e449e5fa77d902a4549e663427873bf
joseAvestruz <joseavestruz4@gmail.com>
2026-09-11 00:05:01 +0100

    Add Route Params

--- src/server.js
+++ src/server.js
@@ -2,7 +2,9 @@ const express =  require('express')
 const app = express()
 const port = 3333
 
-app.get('/',(require,response)=>{
-    response.send('Hello Word With Node.js !')
+app.get('/message/:id/:user',(require,response)=>{
+    const {id , user} = require.params
+    response.send(`Id do usuario e ${id} com o nome ${user}`)
 })
-app.listen(port,()=>{ console.log(`Server is Running On Port ${port}`)})
+
+app.listen(port,()=>{ console.log(`Server is Running On Por ${port}`)});
