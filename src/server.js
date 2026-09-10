3bf1657f488c27fe27a7d2556fc120b47fb1bd9d
joseAvestruz <joseavestruz4@gmail.com>
2026-09-10 19:52:59 +0100

    First Hello Word With node.js With Express

--- src/server.js
+++ src/server.js
@@ -2,4 +2,7 @@ const express =  require('express')
 const app = express()
 const port = 3333
 
+app.get('/',(require,response)=>{
+    response.send('Hello Word With Node.js !')
+})
 app.listen(port,()=>{ console.log(`Server is Running On Port ${port}`)})
