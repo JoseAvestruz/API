319301e44c8e8630e061cc50811f3959fd7662c5
joseAvestruz <joseavestruz4@gmail.com>
2026-09-11 10:31:49 +0100

    add NodeMon Lib

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ const app = express()
 const port = 3333
 
 app.get('/message/:id/:user',(require,response)=>{
-    //route Params
+    //route Params Elas sao obrigatorio
     const {id , user} = require.params
     response.send(`Id do usuario e ${id} com o nome ${user}`)
 })
 
-//Route Query
+//Route Query ja e opcinal  esse  basta acessar a rota
 app.get('/user',(request,response)=>{
-    const {page , limit} = request.query
-    response.send(`Pagina ${page}. Mostrar ${limit}`);
+    const {page , limit, category} = request.query
+   // saida  http://localhost:3333/user/?page=2&limit=10
+  // http://localhost:3333/user/?page=2&limit=10&category=jogos
+    response.send(`Pagina ${page}. Mostrar ${limit} categoria ${category}`);
 })
 
 app.listen(port,()=>{ console.log(`Server is Running On Por ${port}`)});
