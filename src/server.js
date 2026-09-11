12be268c0d65d6b4cfe430a124d52be907de9e45
joseAvestruz <joseavestruz4@gmail.com>
2026-09-11 11:15:11 +0100

    utilizando o ensomnia e recebendo as informacoes via APi no formato app.json({})

--- src/server.js
+++ src/server.js
@@ -1,20 +1,10 @@
 const express =  require('express')
 const app = express()
 const port = 3333
-
-app.get('/message/:id/:user',(require,response)=>{
-    //route Params Elas sao obrigatorio
-    const {id , user} = require.params
-    response.send(`Id do usuario e ${id} com o nome ${user}`)
+app.use(express.json())
+app.post('/user',(request,response)=>{
+    const {name, email, password} = request.body
+    response.json({name, email, password})
 })
-
-//Route Query ja e opcinal  esse  basta acessar a rota
-app.get('/user',(request,response)=>{
-    const {page , limit, category} = request.query
-   // saida  http://localhost:3333/user/?page=2&limit=10
-  // http://localhost:3333/user/?page=2&limit=10&category=jogos
-    response.send(`Pagina ${page}. Mostrar ${limit} categoria ${category}`);
-})
-
 app.listen(port,()=>{ console.log(`Server is Running On Por ${port}`)});
 
