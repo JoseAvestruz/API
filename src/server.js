5230b3c1428a38fba9e5811dc08e1fc264f1f618
joseAvestruz <joseavestruz4@gmail.com>
2026-09-11 10:24:50 +0100

    add Route Query

--- src/server.js
+++ src/server.js
@@ -3,8 +3,15 @@ const app = express()
 const port = 3333
 
 app.get('/message/:id/:user',(require,response)=>{
+    //route Params
     const {id , user} = require.params
     response.send(`Id do usuario e ${id} com o nome ${user}`)
+})
+
+//Route Query
+app.get('/user',(request,response)=>{
+    const {page , limit} = request.query
+    response.send(`Pagina ${page}. Mostrar ${limit}`);
 })
 
 app.listen(port,()=>{ console.log(`Server is Running On Por ${port}`)});
